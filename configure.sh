2e8612a5051e24c1f83a62f07fce9874aaadeb12
Evelthon Prodromou <epro@prodromou.eu>
2019-11-15 10:02:04 +0200

    Added daily power-on at 07:30

--- configure.sh
+++ configure.sh
@@ -28,6 +28,10 @@ fi
 sudo installer -verbose -pkg Kyocera\ OS\ X\ 10.5+\ build\ 2016.05.15.pkg -target /
 
 
+# Wake-up/Boot daily at 07:30
+sudo pmset repeat wakeorpoweron MTWRFSU 07:30:00
+
+
 for home in $(ls /Users | grep -v localadmin | grep -v Shared | grep -v library | grep -v admin)
 do
 #echo $home
