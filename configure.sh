b6fc4b7bc952d547f295e73461107e4c96897d50
Evelthon Prodromou <epro@prodromou.eu>
2019-11-24 00:57:23 +0200

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -79,6 +79,8 @@ chmod 644 /Library/LaunchAgents/com.papercut.client.plist /Library/LaunchDaemons
 # Show input menu in menu bar
 #  defaults write  com.apple.TextInputMenu visible -int 1
 
+# Caps lock switch lang
+# defaults write com.apple.TextInputMenuAgent "NSStatusItem Visible Item-0" -int 0
 
 # Final step
 echo "\n\n################################################"
