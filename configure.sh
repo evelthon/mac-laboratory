3ef5553bf584449f14f1679443563223e104a500
Evelthon Prodromou <epro@prodromou.eu>
2019-11-24 00:46:35 +0200

    input menu command

--- configure.sh
+++ configure.sh
@@ -76,6 +76,9 @@ chmod 644 /Library/LaunchAgents/com.papercut.client.plist /Library/LaunchDaemons
 # 15 0 * * * sudo /Users/admin/scripts/shutdown.sh >/dev/null 2>&1 
 (crontab -l 2>/dev/null; echo "15 0 * * * sudo /Users/admin/scripts/shutdown.sh >/dev/null 2>&1 ") | crontab - admin
 
+# Show input menu in menu bar
+#  defaults write  com.apple.TextInputMenu visible -int 1
+
 
 # Final step
 echo "\n\n################################################"
